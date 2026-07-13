787bca2d5b8e82312227df9d6120880f59ac2902
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-07-13 21:24:49 +0800

    NeoForge 1.21 - 1.21.1

---
+++ src/main/java/gc/mc/mf3f/mixin/PlayerMixin.java
@@ -0,0 +1,17 @@
+package gc.mc.mf3f.mixin;
+
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    @ModifyConstant(
+            method = "getDigSpeed",
+            constant = @Constant(floatValue = 0.0027F)
+    )
+    private float modifyMiningFatigueCase2(float original) {
+        return 0.027F;
+    }
+}
