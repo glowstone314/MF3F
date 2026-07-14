d8d4eefa59041483e2427e23eab463debe0ba31c
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-07-14 16:13:44 +0800

    Fix the destroy speed for Mining Fatigue 4 and above

--- src/main/java/gc/mc/mixin/PlayerEntityMixin.java
+++ src/main/java/gc/mc/mixin/PlayerEntityMixin.java
@@ -1,17 +1,50 @@
 package gc.mc.mixin;
 
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.effect.MobEffectUtil;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
-public class PlayerEntityMixin {
-    @ModifyConstant(
+public abstract class PlayerEntityMixin {
+    @Inject(
             method = "getDestroySpeed",
-            constant = @Constant(floatValue = 0.0027F)
+            at = @At("HEAD"),
+            cancellable = true
     )
-    private float modifyMiningFatigueCase2(float original) {
-        return 0.027F;
+    private void overrideGetDestroySpeed(BlockState state, CallbackInfoReturnable<Float> cir) {
+        Player player = (Player) (Object) this;
+
+        float speed = player.getInventory().getSelectedItem().getDestroySpeed(state);
+        if (speed > 1.0F) {
+            speed += (float) player.getAttributeValue(Attributes.MINING_EFFICIENCY);
+        }
+
+        if (MobEffectUtil.hasDigSpeed(player)) {
+            speed *= 1.0F + (float) (MobEffectUtil.getDigSpeedAmplification(player) + 1) * 0.2F;
+        }
+
+        if (player.hasEffect(MobEffects.MINING_FATIGUE)) {
+            int amplifier = player.getEffect(MobEffects.MINING_FATIGUE).getAmplifier();
+            float scale = (float) Math.pow(0.3, amplifier + 1);
+            speed *= scale;
+        }
+
+        speed *= (float) player.getAttributeValue(Attributes.BLOCK_BREAK_SPEED);
+        if (player.isEyeInFluid(FluidTags.WATER)) {
+            speed *= (float) player.getAttribute(Attributes.SUBMERGED_MINING_SPEED).getValue();
+        }
+
+        if (!player.onGround()) {
+            speed /= 5.0F;
+        }
+
+        cir.setReturnValue(speed);
     }
 }
