a12a3be2d797451d1cc7b89246eb27ec091b9c1f
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-07-15 18:03:41 +0800

    Forge 1.20.6

---
+++ src/main/java/gc/mc/mf3f/MF3F.java
@@ -0,0 +1,10 @@
+package gc.mc.mf3f;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MF3F.MODID)
+public class MF3F {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "mf3f";
+}
---
+++ src/main/java/gc/mc/mf3f/event/MiningSpeedHandler.java
@@ -0,0 +1,32 @@
+package gc.mc.mf3f.event;
+
+import gc.mc.mf3f.MF3F;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MF3F.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class MiningSpeedHandler {
+
+    @SubscribeEvent
+    public static void onBreakSpeed(PlayerEvent.BreakSpeed event) {
+        Player player = event.getEntity();
+        float speed = event.getOriginalSpeed();
+
+        if (player.hasEffect(MobEffects.DIG_SLOWDOWN)) {
+            int amplifier = player.getEffect(MobEffects.DIG_SLOWDOWN).getAmplifier();
+            if (amplifier == 2) {
+                speed /= 0.0027F;
+                speed *= 0.027F;
+            } else if (amplifier >= 3) {
+                speed /= 8.1E-4F;
+                float scale = (float) Math.pow(0.3, amplifier + 1);
+                speed *= scale;
+            }
+        }
+
+        event.setNewSpeed(speed);
+    }
+}
