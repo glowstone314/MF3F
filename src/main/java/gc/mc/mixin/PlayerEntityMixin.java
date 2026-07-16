6659ebdc44209a793da17eaa232c62b112ff6a31
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-07-16 13:43:19 +0800

    Change the implementation to intercept the return value

--- src/main/java/gc/mc/mixin/PlayerEntityMixin.java
+++ src/main/java/gc/mc/mixin/PlayerEntityMixin.java
@@ -1,11 +1,8 @@
 package gc.mc.mixin;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.effect.StatusEffectUtil;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.registry.tag.FluidTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,34 +12,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class PlayerEntityMixin {
     @Inject(
             method = "getBlockBreakingSpeed",
-            at = @At("HEAD"),
+            at = @At("RETURN"),
             cancellable = true
     )
-    private void overrideGetDestroySpeed(BlockState state, CallbackInfoReturnable<Float> cir) {
+    private void onGetDestroySpeed(BlockState state, CallbackInfoReturnable<Float> cir) {
         PlayerEntity player = (PlayerEntity) (Object) this;
 
-        float speed = player.getInventory().getSelectedStack().getMiningSpeedMultiplier(state);
-        if (speed > 1.0F) {
-            speed += (float) player.getAttributeValue(EntityAttributes.MINING_EFFICIENCY);
-        }
-
-        if (StatusEffectUtil.hasHaste(player)) {
-            speed *= 1.0F + (float)(StatusEffectUtil.getHasteAmplifier(player) + 1) * 0.2F;
-        }
+        float speed = cir.getReturnValue();
 
         if (player.hasStatusEffect(StatusEffects.MINING_FATIGUE)) {
             int amplifier = player.getStatusEffect(StatusEffects.MINING_FATIGUE).getAmplifier();
-            float scale = (float) Math.pow(0.3, amplifier + 1);
-            speed *= scale;
-        }
 
-        speed *= (float)player.getAttributeValue(EntityAttributes.BLOCK_BREAK_SPEED);
-        if (player.isSubmergedIn(FluidTags.WATER)) {
-            speed *= (float)player.getAttributeInstance(EntityAttributes.SUBMERGED_MINING_SPEED).getValue();
-        }
-
-        if (!player.isOnGround()) {
-            speed /= 5.0F;
+            if (amplifier == 2) {
+                speed /= 0.0027F;
+                speed *= 0.027F;
+            } else if (amplifier >= 3) {
+                speed /= 8.1E-4F;
+                float scale = (float) Math.pow(0.3, amplifier + 1);
+                speed *= scale;
+            }
         }
 
         cir.setReturnValue(speed);
