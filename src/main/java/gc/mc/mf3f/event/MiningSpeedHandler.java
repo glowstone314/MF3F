6ac8c71e0d52a230ce63ceb8c77590d0bf4e90e3
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-07-17 12:30:43 +0800

    Forge 1.13.2

---
+++ src/main/java/gc/mc/mf3f/event/MiningSpeedHandler.java
@@ -0,0 +1,33 @@
+package gc.mc.mf3f.event;
+
+import gc.mc.mf3f.MF3F;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MF3F.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class MiningSpeedHandler {
+
+    @SubscribeEvent
+    public static void onBreakSpeed(PlayerEvent.BreakSpeed event) {
+        EntityPlayer player = event.getEntityPlayer();
+        float speed = event.getOriginalSpeed();
+
+        Potion MINING_FATIGUE = Potion.getPotionById(4);
+        if (player.isPotionActive(MINING_FATIGUE)) {
+            int amplifier = player.getActivePotionEffect(MINING_FATIGUE).getAmplifier();
+            if (amplifier == 2) {
+                speed /= 0.0027F;
+                speed *= 0.027F;
+            } else if (amplifier >= 3) {
+                speed /= 8.1E-4F;
+                float scale = (float) Math.pow(0.3, amplifier + 1);
+                speed *= scale;
+            }
+        }
+
+        event.setNewSpeed(speed);
+    }
+}
